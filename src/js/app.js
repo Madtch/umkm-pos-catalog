49a6d88212cc740879e68765a069323c9c171fcf
Zamzami Ahmad <zamzamiahmd2112@gmail.com>
2026-08-03 19:35:42 +0700

    add cart and new ui

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import { products } from "./data.js";
 let cart = [];
 const searchInput = document.getElementById("searchInput");
 const container = document.querySelector(".container");
+const cartContainer = document.getElementById("cartContainer");
 
 function renderProduct(dataArray) {
   let htmlContent = "";
@@ -17,7 +18,7 @@ function renderProduct(dataArray) {
     dataArray.forEach((produk) => {
       htmlContent += `
       <div class="card bg-white rounded-xl shadow-md p-5 flex flex-col justify-between">
-      <span class="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded">${produk.category}</span
+      <span class="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded">${produk.category}</span>
       <h3 class="text-lg font-bold text-gray-800 my-1">${produk.name}</h3>
       <p class="text-gray-600 font-medium mb-4">Harga: Rp ${produk.price}</p>
       <button class="bg-blue-600 text-white font-medium w-full py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors" data-id="${produk.id}">Tambah ke Keranjang</button> 
@@ -59,4 +60,53 @@ container.addEventListener("click", (e) => {
     }
   }
   console.log(cart);
+  renderCart();
 });
+
+function renderCart() {
+  let htmlCart = "";
+
+  // (Keranjang Kosong)
+  if (cart.length === 0) {
+    cartContainer.innerHTML = `
+    <h2 class="text-xl font-bold mb-4 border-b pb-2">Keranjang Belanja</h2>
+    <p class="text-gray-400 text-center py-8">Keranjang masih kosong</p>
+    `;
+    return; // Berhenti
+  }
+
+  // Jika Keranjang Ada Isinya
+  htmlCart = `<h2 class="text-xl font-bold mb-4 border-b pb-2">Keranjang Belanja</h2>`;
+  let total = 0;
+
+  cart.forEach((item) => {
+    total += item.price * item.quantity; // Hitung total DI DALAM loop
+    htmlCart += `
+    <div class="flex justify-between items-center border-b pb-3 mb-3">
+    <div>
+    <h4 class="font-bold text-gray-800">${item.name}</h4>
+    <p class="text-sm text-gray-500">Rp ${item.price} x ${item.quantity}</p>
+    </div>
+    <span class="font-bold text-blue-600">Rp ${item.price * item.quantity}</span>
+    </div>
+    `;
+  });
+
+  // Tambahkan Total & Tombol Checkout
+  htmlCart += `
+  <div class="mt-6 pt-4 border-t border-gray-200">
+    <div class="flex justify-between items-center text-lg font-bold mb-4">
+      <span>Total:</span>
+      <span class="text-blue-600">Rp ${total}</span>
+    </div>
+    <button class="w-full bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-4 rounded-lg transition-colors">
+      Checkout / Bayar
+    </button>
+  </div>
+`;
+
+  // Render ke DOM
+  cartContainer.innerHTML = htmlCart;
+}
+
+renderCart();
